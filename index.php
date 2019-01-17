4e56447db26826783b9467be0fdf8f406687c7ba
Ranj Bahadin <ranjbahadin@live.se>
2019-01-17 09:58:30 +0100

    adding form

--- index.php
+++ index.php
@@ -4,9 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Naturreservatet</title>
 </head>
 <body>
+    <?php
+
+    ?>
+    <form action="results.php" method="POST">
+        <h2>Hur många av varje vill du ha i reservatet?</h2><br>
+        <label>Apor?</label><br>
+        <input type=text name=apes><br><br>
+
+        <label>Giraffer?</label><br>
+        <input type=text name=girafs><br><br>
+
+        <label>Tigrar?</label><br>
+        <input type=text name=tiger><br><br>
+
+        <label>Kokosnötter?</label><br>
+        <input type=text name=coconuts><br><br>
+        <button type=submit>Skicka</button><br>
+
+    </form>
     
 </body>
 </html>
